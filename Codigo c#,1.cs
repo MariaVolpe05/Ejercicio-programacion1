2b70ba6f30d414ef9a5dd84bfd703eb19e83e470
MariaVolpe05 <78612759+MariaVolpe05@users.noreply.github.com>
2021-02-05 19:57:26 -0400

    Ejercicio programacion 1 Maria Volpe
    Cualquier inconveniente me avisa profe :)

---
+++ Codigo c#,1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Se formaran distintas combinaciones numericas con los siguientes numeros:");
+            Console.WriteLine("a siendo 2, b siendo 4 y c siendo 1");
+
+            //Primeramente asignare los valores a cada variable:
+
+            int a = 2;
+            int b = 4;
+            int c = 1;
+            //Utilizo int ya que no necesitamos alguno con valores decimales, solo encontramos y trabajaremos en esto en numeros enteros
+
+            Console.WriteLine("comenzaremos con la primera combinacion aacb, la cual quedaria de la siguiente forma:");
+
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.Write(b);
+
+            Console.WriteLine("");
+            //le coloco esto para poder tener espaciado entre lineas
+
+
+            Console.WriteLine("Luego pasaremos a la siguiente combinacion bca, la cual quedaria de la siguiente forma:");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(a);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Por ultimo tendremos la siguiente combinacion bcbc, la cual quedaria de la siguiente forma:");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.Write(c);
+
+            Console.WriteLine("");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
